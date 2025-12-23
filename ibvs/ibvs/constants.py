d21349710a8e2d75d0cb2075f1c134aedc259756
Matthew Troy P <matthew.troy271@gmail.com>
2025-12-24 04:32:59 +0700

    Update constants.py

--- ibvs/ibvs/constants.py
+++ ibvs/ibvs/constants.py
@@ -34,6 +34,8 @@ QGC_PORT = 5600
 R_CB = np.array([[0,0,1],[1,0,0],[0,1,0]], dtype=float)
 P_CB = np.array([P_CB_X, P_CB_Y, P_CB_Z])
 
+MAX_OFFSET = 0.1
+
 MAX_TAU_Z = 0.3
 MAX_TAU_YAW = 0.3
 MAX_TAU_X = 0.3
