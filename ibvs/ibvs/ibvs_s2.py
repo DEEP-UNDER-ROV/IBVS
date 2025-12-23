7628fea99627433908e8d95d35bd1fa8702744af
Matthew Troy P <matthew.troy271@gmail.com>
2025-12-23 19:42:49 +0700

    Update ibvs_s2.py

--- ibvs/ibvs/ibvs_s2.py
+++ ibvs/ibvs/ibvs_s2.py
@@ -13,15 +13,16 @@ from ibvs.constants import *
 R_CB = np.array([[0,0,1],[1,0,0],[0,1,0]], dtype=float)
 p_CB = np.array([P_CB_X, P_CB_Y, P_CB_Z])
 
-class IBVSIntegratedNode(Node):
+class IBVSControllerNode(Node):
     def __init__(self):
-        super().__init__("ibvs_integrated_node")
+        super().__init__("IBVSControllerNode")
         self.bridge = CvBridge()
-        self.sub_corners = self.create_subscription(
-            PolygonStamped, "/apriltag/corners", self.cb_corners, 10)
-        self.sub_depth = self.create_subscription(
-            Image, "/camera/depth/image_raw", self.cb_depth, 10)
 
+        #Subscriber
+        self.sub_corners = self.create_subscription(PolygonStamped, "/apriltag/corners", self.cb_corners, 10)
+        self.sub_depth = self.create_subscription(Image, "/camera/depth/image_raw", self.cb_depth, 10)
+
+        #Publisher
         self.vel_pub = self.create_publisher(Twist, "/ibvs/vel", 10)
         self.pos_pub = self.create_publisher(Point, "/ibvs/pos", 10)
         self.err_pub = self.create_publisher(Float32MultiArray, "/ibvs/error", 10)
@@ -31,51 +32,83 @@ class IBVSIntegratedNode(Node):
         self.last_time = self.get_clock().now()
         self.desired_pts = self._make_desired_pts()
 
-    def _make_desired_pts(self):
-        s = DESIRED_SIZE // 2
-        return np.array([[CX-s,CY-s],[CX+s,CY-s],[CX+s,CY+s],[CX-s,CY+s]])
+    def desired_corners(self, Z_DES , fx, fy, cx, cy, tag_size):
+        half = tag_size / 2.0
+        corners_3d = np.array([
+            [-half, -half, Z_DES],
+            [ half, -half, Z_DES],
+            [ half,  half, Z_DES],
+            [-half,  half, Z_DES],
+        ])
+
+        desired = np.zeros((4, 2), dtype=float)
+    
+        for i, (X, Y, Z) in enumerate(corners_3d):
+            u = fx * (X / Z) + cx
+            v = fy * (Y / Z) + cy
+            desired[i] = [u, v]
+    
+        return desired
 
     def cb_depth(self,msg):
         self.depth_img = self.bridge.imgmsg_to_cv2(msg).astype(np.float32)*0.001
 
-    def interaction_matrix(self,u,v,Z):
-        x,y=(u-CX)/FX,(v-CY)/FY
+    def interaction_matrix(self, u, v, Z):
+        x = (u - CX) / FX
+        y = (v - CY) / FY
         return np.array([
-            [-1/Z,0,x/Z,x*y,-(1+x*x),y],
-            [0,-1/Z,y/Z,1+y*y,-x*y,-x],
-            [0,0,-1,-y*Z,x*Z,0]
+            [-1/Z,  0,    x/Z,  x*y,      -(1+x*x),  y],
+            [0,    -1/Z,  y/Z,  1+y*y,    -x*y,     -x],
+            [0,     0,   -1,   -y*Z,       x*Z,      0]
         ])
 
     def cb_corners(self,msg):
-        if self.depth_img is None: return
+        if self.depth_img is None: 
+            return
+            
         now = self.get_clock().now()
         dt = (now-self.last_time).nanoseconds*1e-9
         self.last_time = now
-        if dt<=0: return
+        if dt<=0: 
+            return
+
+        self.desired_pts = self.desired_corners(Z_DES, FX, FY, CX, CY, TAG_SIZE)
 
         rows,errs=[],[]
-        pts=np.array([[p.x,p.y] for p in msg.polygon.points])
-        h,w=self.depth_img.shape
+        pts = np.array([[p.x,p.y] for p in msg.polygon.points])
+        h,w = self.depth_img.shape
 
         for i,(u,v) in enumerate(pts):
             ui,vi=int(u),int(v)
-            Z=np.median(self.depth_img[vi-PATCH:vi+PATCH+1,ui-PATCH:ui+PATCH+1])
+            
+            Z = np.median(self.depth_img[
+                max(0, vi-PATCH):vi+PATCH+1,
+                max(0, ui-PATCH):ui+PATCH+1
+                ])
+            
+            if Z <= 0 or np.isnan(Z):
+                return
+                
             rows.append(self.interaction_matrix(u,v,Z))
-            cx,cy=(u-CX)/FX,(v-CY)/FY
-            dx,dy=(self.desired_pts[i]-[CX,CY])/[FX,FY]
-            errs.extend([cx-dx,cy-dy,Z-Z_DES])
+            x,y = (u-CX)/FX, (v-CY)/FY
+            ud, vd = self.desired_pts[i]
+            xd,yd = (ud - CX) / FX, (vd - CY) / FY
+            errs.extend([x - xd, y - yd, Z - Z_DES])
 
-        Vc=-LAMBDA_P*np.linalg.pinv(np.vstack(rows))@np.array(errs).reshape(-1,1)
-        v_b=R_CB@Vc[0:3]
+        L = np.vstack(rows)                    # (12x6)
+        e = np.array(errs).reshape(-1, 1)      # (12x1)
 
-        self.p_cmd += v_b.flatten()*dt
-        self.p_cmd = np.clip(self.p_cmd,-MAX_OFFSET,MAX_OFFSET)
+        Vc = -LAMBDA_P * np.linalg.pinv(L) @ e
+        Vb = R_CB @ Vc[0:3]
+
+        self.p_cmd += Vb.flatten() * dt
+        self.p_cmd = np.clip(self.p_cmd, -MAX_OFFSET, MAX_OFFSET)
 
         vel=Twist()
-        vel.linear.x,vel.linear.y,vel.linear.z=v_b.flatten()
+        vel.linear.x,vel.linear.y,vel.linear.z=Vb.flatten()
         self.vel_pub.publish(vel)
 
-        self.pos_pub.publish(Point(x=self.p_cmd[0],y=self.p_cmd[1],z=self.p_cmd[2]))
+        self.pos_pub.publish(Point(x = self.p_cmd[0],y = self.p_cmd[1],z = self.p_cmd[2]))
         self.err_pub.publish(Float32MultiArray(data=np.array(errs,dtype=np.float32).tolist()))
 
 def main():
