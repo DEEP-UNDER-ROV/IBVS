4d53858d02a420711a43760223e9e8acff22c6af
Matthew Troy P <matthew.troy271@gmail.com>
2026-02-27 01:43:16 +0700

    Update constants.py

--- ibvs 2.0/ibvs/constants.py
+++ ibvs 2.0/ibvs/constants.py
@@ -1,35 +1,50 @@
 import numpy as np
 
-# ibvs_control/constants.py
-# --- Camera Matrix (Intrinsics) from RGB Column ---
+# --- Camera Matrix (Intrinsics)
 FX = 634.390
 FY = 636.006
 CX = 654.090
 CY = 369.740
 
+
 # --- Distortion Coefficients (k1, k2, p1, p2, k3) ---
-# Note: k3 is not in your table, so we use 0.0
 DIST_COEFFS = [-0.053665, 0.0314185, -0.00216851, 0.00262024, 0.0]
 
-# --- BlueROV2 Physical Offsets (Camera relative to CoG) ---
+
+# --- ASEPP Physical Offsets (Camera relative to CoG) ---
 P_CB_X = -0.17
 P_CB_Y = 0.0
 P_CB_Z = 0.02
 
+
 # --- IBVS Control Parameters ---
-LAMBDA_P = 0.8
-Z_DES = 1.0
+LAMBDA_P = np.array([0.8, 0.8, 0.6, 0.0, 0.0, 0.2]) # Surge - Sway - Heave - Roll - Pitch - Yaw
+mu = 0.2
+Z_DES = 1.2
 TAG_SIZE = 0.207
 PATCH = 2
 
+W_diag = np.array([1.0, 1.0, 0.2])
+# W = np.diag(np.tile(W_diag, 4))
+W = np.kron(np.eye(4), W_diag)
+
+
+
 # --- Velocity Limits ---
-MAX_LIN_VEL = 0.3
-MAX_ANG_VEL = 0.4
+MAX_LIN_VEL = 0.5
+MAX_ANG_VEL = 0.2
+
 
 # --- QGC Port Configuration ---
 QGC_IP = "192.168.4.1"
 QGC_PORT = 5600
 
+
 # --- Transformation Matrix ---
-R_CB = np.array([[0,0,1],[1,0,0],[0,1,0]], dtype=float)
+# Camera X right | Y down  | Z front
+# U-ROVs X front | Y right | Z down
+R_CB = np.array([[0,0,1],
+                 [1,0,0],
+                 [0,1,0]], dtype=float) # CAM to NED
+
 P_CB = np.array([P_CB_X, P_CB_Y, P_CB_Z])
