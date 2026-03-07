8c25d167b9a1a3e9a3ca1a45ae45d5fb9bc7521b
Matthew Troy P <matthew.troy271@gmail.com>
2026-03-07 10:07:16 +0700

    Update ibvs2.py

--- ibvs 2.0/ibvs/ibvs2.py
+++ ibvs 2.0/ibvs/ibvs2.py
@@ -1,4 +1,4 @@
-# IBVS Normal LS 2 Stacked
+# IBVS Full LS 3 Stacked Normalized Z
 
 #!/usr/bin/env python3
 import rclpy
@@ -34,17 +34,17 @@ class IBVSRCController(Node):
         self.depth_img = None
         self.last_tag_time = None
         self.tag_lost = True
-        self.TAG_TIMEOUT = 2.5  # seconds
+        self.TAG_TIMEOUT = 0.5  # seconds
 
         # RC command buffer (IMPORTANT)
         self.rc_cmd = [1500] * 18
 
         # ---------------- Gains (TUNE IN WATER) ----------------
-        self.K_SURGE = 800
-        self.K_SWAY  = 1500
-        self.K_HEAVE = 500
+        self.K_SURGE = 500
+        self.K_SWAY  = 1200
+        self.K_HEAVE = 750
         self.K_YAW   = 200
-        self.HEAVE_BIAS = -100
+        self.HEAVE_BIAS = -133
 
         # ---------------- Desired image features ----------------
         self.desired_pts = self.compute_desired_corners(
@@ -77,8 +77,9 @@ class IBVSRCController(Node):
         x = (u - CX) / FX
         y = (v - CY) / FY
         return np.array([
-            [-1/Z,  0,    x/Z,  x*y,      -(1 + x*x),  y],
-            [0,    -1/Z,  y/Z,  1 + y*y,  -x*y,       -x]
+            [-1/Z,  0,    x/Z,      x*y,     -(1 + x*x),  y],
+            [0,    -1/Z,  y/Z,    1 + y*y,      -x*y,    -x],
+            [0,     0, -1/Z_DES, -y*Z/Z_DES,  x*Z/Z_DES,  0]
         ])
 
     # =========================================================
@@ -93,9 +94,9 @@ class IBVSRCController(Node):
         self.last_tag_time = self.get_clock().now()
         self.tag_lost = False
 
-        L_stack = []
-        error_vector = []
-        pixel_err_list = []
+        rows = []
+        errs = []
+        pixel_err = []
 
         for i, p in enumerate(msg.polygon.points):
             u, v, Z = p.x, p.y, p.z
@@ -103,28 +104,25 @@ class IBVSRCController(Node):
                 return
                 
             ud, vd = self.desired_pts[i]
-            pixel_err_list.append(u - ud)
-            pixel_err_list.append(v - vd)
+            pixel_err.extend([u - ud, v - vd])
+            rows.append(self.interaction_matrix(u, v, Z))
 
             x, y = (u - CX)/FX, (v - CY)/FY
             xd, yd = (self.desired_pts[i] - [CX, CY]) / [FX, FY]
+            z_norm = (Z - Z_DES) / Z_DES
+            errs.extend([x - xd, y - yd, z_norm])
+            # errs.extend([x - xd, y - yd, 0.25*(Z - Z_DES)])
 
-            error_vector.append(x - xd)
-            error_vector.append(y - yd)
-            L_stack.append(self.interaction_matrix(u, v, Z))
+        L = np.vstack(rows)
+        e = np.array(errs).reshape(-1, 1)
 
-        L = np.vstack(L_stack)
-        e = np.array(error_vector).reshape(-1, 1)
-
-        A = L.T @ W @ L + mu**2 * np.eye(6)
-        b = L.T @ W @ e
+        A = L.T @ L + mu**2 * np.eye(6)
+        b = L.T @ e
         Vc = - np.diag(LAMBDA_P) @ np.linalg.solve(A,b)
 
-        pixel_err = np.array(pixel_err_list)
         pixel_err_magnitude = np.abs(pixel_err)
         if np.max(pixel_err_magnitude) < 1.5:
             Vc[:] = 0.0
-
 
         Vc[0:3] = np.clip(Vc[0:3], -MAX_LIN_VEL, MAX_LIN_VEL)
         Vc[3:6] = np.clip(Vc[3:6], -MAX_ANG_VEL, MAX_ANG_VEL)
@@ -144,11 +142,25 @@ class IBVSRCController(Node):
         vel_msg.header.frame_id = "body"
         vel_msg.twist.linear.x = float(Vb[0])
         vel_msg.twist.linear.y = float(Vb[1])
-        vel_msg.twist.linear.z = float(-Vb[2])
+        vel_msg.twist.linear.z = float(Vb[2])
         vel_msg.twist.angular.x = float(Wb[0])
         vel_msg.twist.angular.y = float(Wb[1])
         vel_msg.twist.angular.z = float(Wb[2])
         self.vel_pub.publish(vel_msg)
+        
+        self.get_logger().info(
+            f"Surge = {Vb[0]} |"
+            f"Sway = {Vb[1]} |"
+            f"Heave = {Vb[2]} |"
+            f"Yaw Body = {Wb[2]}"
+            f"C1 = {Vc[0]} |"
+            f"C2 = {Vc[1]} |"
+            f"C3 = {Vc[2]} |"
+            f"C4 = {Vc[3]} |"
+            f"C5 = {Vc[4]} |"
+            f"c6 = {Vc[5]} |",
+            throttle_duration_sec=0.5
+        )
 
         # -------- Compute PWM --------
         pwm = [1500] * 18
@@ -174,7 +186,7 @@ class IBVSRCController(Node):
         )
 
         err_msg = Float32MultiArray()
-        err_msg.data = np.array(e, dtype=np.float32).tolist()
+        err_msg.data = np.array(e, dtype=np.float32).flatten().tolist()
         self.err_pub.publish(err_msg)
 
     def publish_rc(self):
